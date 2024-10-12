70e06dc965f9f3c5f4fa1a51396e442bfd218656
Avishka Pasindu <avishkap@mitesp.com>
2024-10-12 22:02:56 +0530

    added basic rabbitmq message consumer producer apps

---
+++ Consumer/Reciever.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Producer
+{
+    public class Reciever
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare("BasicTestQueue", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(chanel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Recieved {0}",message);
+                };
+
+                chanel.BasicConsume("BasicTestQueue", true, consumer);
+                Console.WriteLine("Consuming done");
+            }
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+            using (var connection = factory.CreateConnection()) 
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare("BasicTestQueue", false,false,false,null);
+
+                string message = "Getting started with Rabbitmq2";
+                var body= Encoding.UTF8.GetBytes(message);
+
+                chanel.BasicPublish("", "BasicTestQueue",null,body);
+                Console.WriteLine("Message sent {0} ..." , message); 
+            }
+        }
+    }
+}
